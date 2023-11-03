5d4365d21e765d744173e6a06072cca0e0b28f4a
Mira <mira.tihverainen@gmail.com>
2023-11-03 13:37:33 +0200

    Week 2, task 4 (pylint workflow check working)

--- src/index.py
+++ src/index.py
@@ -2,8 +2,6 @@ from varasto import Varasto
 #add comment to test git conflict
 
 def main(): # pylint: disable=too-many-statements
-    bar = 0
-    print(bar)
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
